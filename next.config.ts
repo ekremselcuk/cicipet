c14ae1a47b8700e41039e7e265080cfd9673bc77
Ekrem <ekremselcuk@gmail.com>
2026-01-28 17:36:48 +0300

    Fix: Remove middleware.ts and revert config to resolve 500 error

--- next.config.ts
+++ next.config.ts
@@ -13,18 +13,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { webpack, isServer, nextRuntime }) => {
-    if (nextRuntime === 'edge') {
-      config.resolve.alias['ws'] = false;
-      config.resolve.alias['buffer'] = false;
-      config.externals = config.externals || [];
-      config.externals.push({
-        'bufferutil': 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      });
-    }
-    return config;
-  },
 };
 
 export default withPWA(nextConfig);
