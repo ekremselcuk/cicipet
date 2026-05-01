4af3bef65551b1eb23268e4f8ff733e3842af18c
Ekrem <ekremselcuk@gmail.com>
2026-05-01 08:57:03 +0300

    fix: next-auth v5 beta'dan v4'e geçildi

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,12 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  trustHost: true,
-  providers: [Google],
-});
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
