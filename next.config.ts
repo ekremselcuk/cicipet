63aecac1fd1c733825581efa2c4e9a9d9caa346d
Ekrem <ekremselcuk@gmail.com>
2026-05-13 23:08:54 +0300

    config: images unoptimized:true eklendi, remotePatterns kaldırıldı

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-    ],
+    unoptimized: true,
   },
 };
 
